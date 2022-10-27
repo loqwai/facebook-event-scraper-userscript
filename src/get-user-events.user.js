38862b1737f5ff85db85b5cf4bd97fb104949408
redaphid <iam@aaronherres.com>
2022-10-27 16:53:01 -0700

    copy ex

---
+++ src/get-user-events.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Facebook Event Scraper
+// @namespace   Violentmonkey Scripts
+// @match       https://www.facebook.com/events
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 10/27/2022, 3:59:32 PM
+// ==/UserScript==
+console.log('hi')
+console.log('there')
+console.log('how')
+console.log('are')
+console.log('you')
+console.log('i')
